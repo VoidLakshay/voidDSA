5a70716fca01a58be7818037080872773befdea0
lakshayvashisth09-netizen <lakshayvashisth09@gmail.com>
2026-08-01 16:08:01 +0530

    Solve trapping rain water

---
+++ linear/array/trappingRainWater.js
@@ -0,0 +1,32 @@
+function trap(height) {
+    if (height.length === 0) return 0;
+    
+    let left = 0;
+    let right = height.length - 1;
+    let leftMax = 0;
+    let rightMax = 0;
+    let totalWater = 0;
+    
+    while (left < right) {
+        if (height[left] < height[right]) {
+            if (height[left] >= leftMax) {
+                leftMax = height[left];
+            } else {
+                totalWater += leftMax - height[left];
+            }
+            left++;
+        } else {
+            if (height[right] >= rightMax) {
+                rightMax = height[right];
+            } else {
+                totalWater += rightMax - height[right];
+            }
+            right--;
+        }
+    }
+    
+    return totalWater;
+}
+
+const heights = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1];
+console.log("Trapped rain water:", trap(heights));
