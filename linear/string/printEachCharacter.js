38ea5db25ffad2f41d667d3c02ca6a01e61f0355
lakshayvashisth09-netizen <lakshayvashisth09@gmail.com>
2026-08-03 21:53:50 +0530

    Solve print each character on new line

---
+++ linear/string/printEachCharacter.js
@@ -0,0 +1,9 @@
+function printEachChar(str) {
+    for (let i = 0; i < str.length; i++) {
+        console.log(str[i]);
+    }
+}
+
+const str = "hello";
+console.log("Characters on new line:");
+printEachChar(str);
